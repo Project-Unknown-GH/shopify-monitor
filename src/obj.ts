edeeb6fcbfcecbc5c1697f20c29863b16948e9cd
xWafl <73sampleperson@gmail.com>
2020-05-30 19:13:13 -0400

    :bug: changed a link

--- src/obj.ts
+++ src/obj.ts
@@ -1,7 +1,7 @@
 import { main } from "./webhook";
 
 const urls = [
-    ["https://eflash-us.doverstreetmarket.com/", "https://discordapp.com/api/webhooks/716362775617863760/YreueDZ1s5bgKe6jXAXdLl84PbwdS8w7yVNVO1sP7N9E-10u5RhVLZMfS7BGqUBnBnJR"],
+    ["https://kith.com/collections/mens-footwear", "https://discordapp.com/api/webhooks/716362775617863760/YreueDZ1s5bgKe6jXAXdLl84PbwdS8w7yVNVO1sP7N9E-10u5RhVLZMfS7BGqUBnBnJR"],
     ["https://undefeated.com/collections/mens-footwear", "https://discordapp.com/api/webhooks/716362702192377856/LhHQfaiC24w4urcH7tmkoAodQvvXx594pKLrRCBeHjfEvmtsSfayR-Dk5ppSPjUK74LD"]
 ];
 
