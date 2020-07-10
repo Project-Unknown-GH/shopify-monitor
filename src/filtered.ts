b84542db7f3d19a038fcff555f013c864e66a97f
xWafl <73sampleperson@gmail.com>
2020-07-10 10:21:00 -0400

    urls

--- src/filtered.ts
+++ src/filtered.ts
@@ -1,20 +1,26 @@
 import { main } from "./webhook";
 
-const urls = [
+const urls = <string[]>[
+    "https://undefeated.com/",
+    "https://extrabutterny.com/",
+    "https://stay-rooted.com/",
+    "https://www.deadstock.ca/",
+    "https://nrml.ca/",
+    "https://shop.havenshop.com/",
+    "https://www.socialstatuspgh.com/",
+    "https://cncpts.com/",
+    "https://us.bape.com/",
+    "https://www.jimmyjazz.com",
+    "https://shopnicekicks.com",
+    "https://www.unknwn.com/",
+    "https://juicestore.com/",
+    "https://feature.com/",
+    "https://www.saintalfred.com/",
+    "https://www.dtlr.com/",
+    "https://www.trophyroomstore.com/",
+    "https://www.noirfonce.eu/",
     "https://kith.com/",
-    // "https://undefeated.com/",
-    // "https://www.deadstock.ca/",
-    // "https://nrml.ca/",
-    // "https://shop.havenshop.com/",
-    // "https://www.socialstatuspgh.com/",
-    // "https://eflash.doverstreetmarket.com/",
-    // "https://www.unknwn.com/",
-    // "https://juicestore.com/",
-    // "https://feature.com/",
-    // "https://www.saintalfred.com/",
-    // "https://www.dtlr.com/",
-    // "https://www.trophyroomstore.com/",
-    // "https://www.noirfonce.eu/"
+    "https://shopnicekicks.com/collections/mens-kicks/"
 ];
 
 const webhook = "https://discordapp.com/api/webhooks/726921036171444254/brX9g-XWlTDS6CdJwmkE5AUGXx_SDiMsVLj7v7TaHggxRo0Q8vfawZd0mw8KfIt_0QL_";
